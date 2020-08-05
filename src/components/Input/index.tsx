71def7bc2793b0ce055f55f46b3a589be62a127e
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-08-05 15:27:01 -0300

    inicio da criação da pagina de cadastro

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,21 @@
+import React, { InputHTMLAttributes } from 'react'
+
+import './styles.css'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+const Input: React.FC<InputProps> = ({ label, name}) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type="text" id={name} />
+    </div>
+  )
+}
+
+
+
+export default Input
