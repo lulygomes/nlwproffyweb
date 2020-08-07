aa6b7cfcc9b66368f99e50b601653aee44d2d476
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-08-07 12:13:33 -0300

    conexão de TeacherList com a API

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,57 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/39559790?s=460&u=a11f86cfac9ebac821a88a7718fba94b5a67179f&v=4" alt="Luiz Silva" />
+        <img src={teacher.avatar} alt={teacher.name} />
+
         <div>
-          <strong>Luiz Silva</strong>
-          <span>Informática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
+
       </header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          <br /><br />
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          </p>
+      <p> {teacher.bio} </p>
 
       <footer>
+
         <p>
           Preço/hora
-              <strong>R$ 80,00</strong>
+        <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+
+        <a
+          onClick={createNewConnection}
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
+          Entrar em contato
+        </a>
       </footer>
 
 
